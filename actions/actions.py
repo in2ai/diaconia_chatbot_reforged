58e5cb16f8e56509591d4ded5523b5116e007e2c
Luis Galocha <luis.galocha@in2ai.com>
2023-07-07 16:43:25 +0200

    Establece una primera demo sencilla

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,30 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class AgeRegistration(Action):
+
+    EDAD_RIESGO = 0
+
+    def name(self) -> Text:
+        return "age_registration"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        if tracker.latest_message['entities'][0]['value'] < 18:
+            self.EDAD_RIESGO = 1
+        dispatcher.utter_message(
+            text="A continuación, tienes 3 opciones, por favor elige una opción." +
+            "1.- ¿Quieres conocer qué es la trata y cómo te puede afectar?" +
+            "2.- ¿Quieres que hablemos de si en tu caso existe algún riesgo de trata?" +
+            "3.- ¿Quieres pedir ayuda porque crees que te encuentras en peligro?" +
+            "Ahora, presiona 1, 2 o 3.")
+
+        return []
